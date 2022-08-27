54cbcd3bfdb4f5b9bd3bdc56d38809e9a7b12047
Teshell <fayrongeek@gmail.com>
2022-08-27 16:18:35 +0100

    animate text on toggle

--- src/components/task-item.tsx
+++ src/components/task-item.tsx
@@ -1,7 +1,17 @@
 import { Stack, Checkbox, Text, useColorModeValue } from "@chakra-ui/react";
 import { motion, AnimatePresence } from "framer-motion";
+import { useState } from "react";
+
+import styled from "@emotion/styled";
+
+const variants = {
+  open: { opacity: 0.3, x: 10 },
+  closed: { opacity: 1, x: 0 },
+};
 
 const TaskItem = () => {
+  const [checked, setChecked] = useState(false);
+
   return (
     <AnimatePresence>
       <motion.div
@@ -17,19 +27,42 @@ const TaskItem = () => {
           py={3}
           w="full"
           align={"center"}
-          bg={useColorModeValue("gray.100", "dark.secondary")}
+          bg={useColorModeValue("#F1F1F1", "dark.secondary")}
           borderRadius={"3xl"}
         >
-          <Checkbox size="lg" colorScheme={"purple"} />
+          <Checkbox
+            checked={checked}
+            onChange={() => setChecked((checked) => !checked)}
+            size="lg"
+            colorScheme="orange"
+          />
 
-          <Text
-            color={useColorModeValue("gray.700", "whiteAlpha.800")}
-            fontWeight={"bold"}
-            letterSpacing={"widest"}
-            fontSize={"xl"}
+          <motion.div
+            style={{ position: "relative" }}
+            initial={{ x: 0 }}
+            animate={checked ? "open" : "closed"}
+            exit={"closed"}
+            transition={{ type: "spring", stiffness: 100 }}
+            variants={variants}
           >
-            Go to a Cafe
-          </Text>
+            <Text
+              color={useColorModeValue("gray.700", "whiteAlpha.800")}
+              fontWeight={"bold"}
+              letterSpacing={"widest"}
+              fontSize={"xl"}
+            >
+              Go to a Cafe
+            </Text>
+            <motion.div
+              style={{
+                position: "absolute",
+                top: 16,
+              }}
+              initial={{ width: 0, height: 2 }}
+              animate={{ width: "100%", height: 2 }}
+              transition={{ type: "spring", stiffness: 100 }}
+            />
+          </motion.div>
         </Stack>
       </motion.div>
     </AnimatePresence>
